2a9bccc13781833e83508929b88f20b675ebf35e
Tomoghna <tomoghanadey622@gmail.com>
2025-02-17 22:39:24 +0530

    made some changes

--- prime.java
+++ prime.java
@@ -4,10 +4,23 @@ public class prime
 {
 	public static void main(String args[])
 	{
-		int flag=0;
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter the number of terms: ");
+		System.out.print("Enter the number of terms to print: ");
 		int n = sc.nextInt();
-		for(int i=1; i<=n; i++)
+		for(int i=2; i<=n; i++)
 		{
-			if(i/1==0 || i
+			int flag = 0;
+			for(int j=2; j<=i/2; j++)
+			{
+				if(i%j == 0)
+				{
+					flag = 1;
+					break;
+				}
+			}
+			if(flag == 0)
+				System.out.print(i + " ");
+		}
+		sc.close();
+	}
+}
