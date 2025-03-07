b3828148ae87814bf1aae8b316576ac6a1838ef0
Tomoghna <tomoghanadey622@gmail.com>
2025-03-07 21:53:55 +0530

    added a program

---
+++ sample1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class practice
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number: ");
+		int n = sc.nextInt();
+		sc.nextLine();
+		System.out.print("What is your name: ");
+		String s = sc.nextLine();
+		System.out.println("Your number is: " + n);
+		System.out.println("Your name is: " + s);
+		
+	}
+}
