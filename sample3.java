6d1c3ef5fa936e19ed6dd0aed71224ad71cc1c9b
Tomoghna <tomoghanadey622@gmail.com>
2025-03-31 18:43:20 +0530

    simple program

---
+++ sample3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Sample3
+{
+	public static void main(String args[]);
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the length of the array: ");
+		int n = sc.nextInt();
+		int arr[] = new arr[n];
+		for(int i=0; i<n; i++)
+		{
+			System.out.print("Enter the "+i+"th element: ");
+			arr[i]=sc.nextInt();
+		}
+		System.out.println("Original Array:");
+		for(int i=0; i<n; i++)
+			System.out.print(i+" ");
+		for(int i)
+	}
+}
